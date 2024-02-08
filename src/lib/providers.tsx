c389132551aaf98443e175ce0dc285ba5a0ea82d
choiruladamm <choiruladamm@gmail.com>
2024-02-08 23:50:18 +0700

    feat: add providers queryClient

---
+++ src/lib/providers.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React, { FC } from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  const [queryClient] = React.useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
